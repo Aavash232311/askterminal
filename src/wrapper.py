11925d7383f648bb07941ed64bd1a0d9422441ee
avash <aavash2005@gmail.com>
2026-05-13 19:06:17 -0500

    Added: prompting LLM from Linux terminal

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,22 +1,23 @@
 import os
 import sys
 from pathlib import Path
+from ollama import chat
 
 
-SCRIPT_PATH = str(Path.cwd()) + "/script/wrapper.py"
+SCRIPT_PATH = str(Path.cwd()) + "/src/wrapper.py"
 BASHRC_PATH = os.path.expanduser("~/.bashrc")
 MARKER = "# >>> mint function >>>"  
 
 FUNCTION_BLOCK = f"""
     {MARKER}
     mint() {{
-        python3 {SCRIPT_PATH} "$@"
+        /home/avash/Desktop/ai_terminal/.venv/bin/python "{SCRIPT_PATH}" "$@"
     }}
     # <<< mint function <
 """
 
 args = sys.argv[1:] 
-
+# Not a serious project.
 
 def already_added():
     if not os.path.exists(BASHRC_PATH):
@@ -29,13 +30,26 @@ def add_function():
         f.write(FUNCTION_BLOCK)
 
 
+from ollama import chat
+
+def llm_call(prompt):
+    response = chat(model='qwen2.5-coder:7b', messages=[
+    {
+        'role': 'user',
+        'content': prompt,
+    },
+    ])
+    print(response.message.content)
+
+
 def main():
     if not already_added():
         add_function()
 
-    print(" Listening in port")
+    prompt = sys.argv[-1]
+    llm_call(prompt=prompt)
+    
 
-
-
+    
 if __name__ == '__main__':
     main()
